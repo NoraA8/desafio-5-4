b0712242230f6383981402dd83ea44a50db5274e
NoraA8 <zaske8@gmail.com>
2023-03-19 21:29:03 -0300

    change h1 on carrito.jsx

--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -3,7 +3,7 @@ import {usePizzaContext} from "../context/PizzaContext"
 export default function Carrito () {
     return (
         <>
-            <h1>Detalles del pedido:</h1>
+            <h1>Carrito vacio</h1>
         </>
     )
 }
